f3846fd8911e329cc22e139d908f5f734e68357a
caesarnetyet <caesarnetyet@gmail.com>
2023-02-06 19:31:36 -0600

    heroes boi

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { Hero } from './hero';
+const HEROES: Hero[] = [
+  { id: 12, name: 'Dr. Nice' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr. IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+
+export class HeroesComponent {
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+  heroes = HEROES;
+}
